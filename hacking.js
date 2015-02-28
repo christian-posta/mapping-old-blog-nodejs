0b057f4f063d74f5cbc52a25c7623e89c06242d0
Christian Posta <christian.posta@gmail.com>
2015-02-28 08:12:15 -0700

    added first part mappings

--- hacking.js
+++ hacking.js
@@ -3,24 +3,19 @@
  */
 var http = require('http');
 var url = require('url');
-var mappings = require('mapping');
+var m = require('./mapping');
 
 http.createServer(function(req,res){
     var query_parts = url.parse(req.url, true);
     var query = query_parts.query;
-    var path = query_parts.pathname;
-
-    if( query.p != undefined && mappings[query.p] != undefined) {
-        console.log("try to redirect to " + mappings[query.p])
-        res.writeHead(302, { 'Location': mappings[query.p]});
-        res.end();
-    }else if(path == '/blog'){
-        res.writeHead(302, { 'Location': 'http://blog.christianposta.com'});
+    var mapping = m.mappings[query.p];
+    if( query.p != null && mapping) {
+        res.writeHead(302, { 'Location': mapping});
         res.end();
     }else {
-        res.write('<html>Maybe looking for <a href="http://blog.christianposta.com">blog.christianposta.com</a>?</html>')
+        res.writeHead(302, { 'Location': 'http://blog.christianposta.com'});
         res.end();
     }
 
-}).listen(9000);
+}).listen(4000);
 console.log("Listenting at http://localhost:9000");
--- mapping.js
+++ mapping.js
@@ -3,7 +3,43 @@
  */
 var baseUrl = 'http://blog.christianposta.com';
 var mappings = {
-    '411' : baseUrl + '/activemq/speeding-up-activemq-persistent-messaging-performance-by-25x/'
+    '441' : baseUrl + '/activemq/speeding-up-activemq-persistent-messaging-performance-by-25x/',
+    '433' : baseUrl + '/camel/very-fast-camels-and-cloud-messaging/',
+    '432' : baseUrl + '/microservices/youre-not-going-to-do-microservices/',
+    '431' : baseUrl + '/activemq/starting-multiple-jboss-a-mq-jvms-on-the-same-machine/',
+    '428' : baseUrl + '/webinar/integrating-microservices-with-apache-camel/',
+    '427' : baseUrl + '/camel/easy-rest-endpoints-with-apache-camel-2-14/',
+    '423' : baseUrl + 'http://blog.christianposta.com/demo/continuous-delivery-with-jenkins-gerrit-jboss-fuse-and-openshift-paas/',
+    '422' : baseUrl + '/interview/developer-interview-3-my-interview-about-hawtio-and-fabric8/',
+    '413' : baseUrl + '/camel/apache-camel-developers-cookbook-ebook-giveaway/',
+    '409' : baseUrl + '/hawtio/hawtio-on-jboss-eap-part-ii/',
+    '403' : baseUrl + '/hawtio/hawtio-on-jboss-wildfly-8-1/',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + '',
+    '' : baseUrl + ''
 };
 
 exports.mappings = mappings;
